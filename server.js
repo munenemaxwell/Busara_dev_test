fdf947bdfe94b39d4eee4b0c684a0142926038c5
maxwell munene <maxwellmunene007@gmail.com>
2018-08-29 18:57:38 +0300

    added cors to expressserver in server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/interview'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/interview/index.html'));
