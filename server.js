8a2ec877440de41e742112d5e6b0688ff86109ac
munenemaxwell <maxwellmunene007@gmail.com>
2018-08-29 09:43:38 +0300

    changes to server.js for heroku deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -14,3 +15,22 @@ res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+*/
+
+const express = require('express');
+const http = require('http')
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
